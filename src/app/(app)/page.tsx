81c8c9ba121caa07bccb0ba39312f4a7af66cf54
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2025-11-09 11:25:11 +0700

    feat: add AI chat background image and update homepage layout according to the design

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { clientApiFetch } from "@/utils/clientApiFetch";
+import Image from "next/image";
 // import type { Metadata } from "next";
 import { useEffect } from "react";
 
@@ -19,5 +21,27 @@ export default function HomePage() {
       console.log(response);
     });
   }, []);
-  return <div className="">Home page</div>;
+  return (
+    <div className="homepage p-2">
+      <div className="flex flex-col items-center justify-center my-8">
+        <Image
+          src={"/ai-chat-bg.jpg"}
+          alt="AI chat background"
+          width={500}
+          height={500}
+          className="rounded-2xl"
+        ></Image>
+        <h1
+          className={`text-[20px] sm:text-[30px] font-extrabold tracking-tight bg-clip-text mt-4`}
+        >
+          Start your first AI conversation
+        </h1>
+        <div className="text-center text-neutral-700 sm:w-[500px] mt-2">
+          Engage with Query Mind to get quick answers, brainstorm ideas, or
+          explore new topics. Your AI conversations will appear here.
+        </div>
+        <Button className="mt-10 px-6 sm:px-8 md:px-10 bg-primary text-primary-foreground">New Conversation</Button>
+      </div>
+    </div>
+  );
 }
