e260836f7dcd8630861211822c7a3ec0f0e56d3d
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2025-11-12 16:35:20 +0700

    fix: update loading skeleton component sizes for better responsiveness; adjust global CSS variable for breakpoints

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -5,13 +5,12 @@ function Loading() {
   return (
     <div className="flex flex-col items-center justify-center my-8">
       <div className="flex flex-col space-y-3 gap-4">
-        <Skeleton className="h-[300px] w-[500px] rounded-xl" />
+        <Skeleton className="h-52 w-72 xs:h-60 xs:w-96 sm:h-[300px] sm:w-[500px] rounded-xl" />
         <div className="space-y-2 flex flex-col gap-4">
-          <Skeleton className="h-[50px] w-[500px]" />
-          <Skeleton className="h-4 w-[500px]" />
-          <Skeleton className="h-4 w-[500px]" />
-          <Skeleton className="h-4 w-[500px]" />
-          <Skeleton className="m-auto mt-4 h-9 w-[200px]" />
+          <Skeleton className="h-12 w-72 xs:h-10 xs:w-96 sm:h-[50px] sm:w-[500px]" />
+          <Skeleton className="h-4 w-72 xs:h-5 xs:w-96 sm:h-[25px] sm:w-[500px]" />
+          <Skeleton className="h-4 w-72 xs:h-5 xs:w-96 sm:h-[25px] sm:w-[500px]" />
+          <Skeleton className="h-8 w-32 xs:h-10 xs:w-40 m-auto mt-4 sm:h-12 sm:w-[200px]" />
         </div>
       </div>
     </div>
