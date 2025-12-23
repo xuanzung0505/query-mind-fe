6543b1f3ed0e0c93528b4a25a360bca1b9c75190
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2025-12-23 14:25:27 +0700

    feat: add metadata for QueryMind chatbot application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import ReactQueryProviderClient from "../components/ReactQueryProviderClient";
 import "./globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Metadata } from "next";
 
 export default function RootLayout({
   children,
@@ -19,3 +20,10 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: "QueryMind chatbot application",
+  description: `QueryMind is an AI chat application, which helps you analyze your documents, query the extracted knowledge 
+  by utilizing a RAG model and third-party LLMs. It thereby helps you to ask anything you like without having to 
+  manually searching pages after pages.`,
+};
